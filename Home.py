955c8094cc82bd999971ba19847e78840e463a9a
msoleimanian <gh.solimanian@gmail.com>
2024-01-15 21:23:22 +0800

    update

--- Home.py
+++ Home.py
@@ -41,34 +41,37 @@ def homepageconstructor():
         st.image("DigitalTwin.jpeg" , width=350)
     st.write('')
 
+    col1 , col2, col3 = st.columns(3)
 
-    html_content = f"""
-                                            <div style="border: 2px solid #333333; padding:10px; border-radius:5px;">
-                                                                <h3 style="color:#333333;">Historical Analyses</h3>
-        1. Comparative Analysis:
-        Explore the performance of different plantings and generations side by side. Our system provides detailed comparisons, highlighting which plants thrived under specific conditions. This feature enables you to make informed decisions based on historical success rates.
-
-    2. Insights and Trends:
-    Dive deep into the trends that shaped your farm's history. Identify the factors contributing to the best performance of your plants and understand how they relate to specific generations. Uncover valuable insights that can guide your future planting strategies.
-
-    3. Generation Exploration:
-    Navigate through the various generations of your crops. Examine the conditions, challenges, and successes of each generation. This feature empowers you to learn from the past and make data-driven decisions to optimize future generations.
-
-
-                                                        """
-    st.markdown(html_content, unsafe_allow_html=True)
-    html_content = f"""
-                                            <div style="border: 2px solid #333333; padding:10px; border-radius:5px;">
-                                                                <h3 style="color:#333333;">Digital Twin</h3>
-
-1. Real-Time Monitoring:
-Witness the live status of your crops through three-dimensional models of your greenhouse. Sensors strategically placed in your farm provide real-time updates on temperature, humidity, soil moisture, and more. Stay connected to your farm like never before.
-
-2. Predictive Analysis:
-Anticipate the future status of your farm with our predictive analysis. By analyzing current conditions and historical data, our system forecasts potential issues and suggests preemptive measures. Proactively manage your farm for optimal performance.
-
-3. Recommendations:
-Receive personalized recommendations based on the real-time data and predictive analysis. Our system guides you on necessary actions to enhance the health and productivity of your crops. From adjusting environmental conditions to suggesting specific interventions, we've got you covered.</p>
-                                                            </div>
-                                                        """
-    st.markdown(html_content, unsafe_allow_html=True)
+    if col2.button('More Info'):
+        html_content = f"""
+                                                <div style="border: 2px solid #333333; padding:10px; border-radius:5px;">
+                                                                    <h3 style="color:#333333;">Historical Analyses</h3>
+            1. Comparative Analysis:
+            Explore the performance of different plantings and generations side by side. Our system provides detailed comparisons, highlighting which plants thrived under specific conditions. This feature enables you to make informed decisions based on historical success rates.
+    
+        2. Insights and Trends:
+        Dive deep into the trends that shaped your farm's history. Identify the factors contributing to the best performance of your plants and understand how they relate to specific generations. Uncover valuable insights that can guide your future planting strategies.
+    
+        3. Generation Exploration:
+        Navigate through the various generations of your crops. Examine the conditions, challenges, and successes of each generation. This feature empowers you to learn from the past and make data-driven decisions to optimize future generations.
+    
+    
+                                                            """
+        st.markdown(html_content, unsafe_allow_html=True)
+        st.write('')
+        html_content = f"""
+                                                <div style="border: 2px solid #333333; padding:10px; border-radius:5px;">
+                                                                    <h3 style="color:#333333;">Digital Twin</h3>
+    
+    1. Real-Time Monitoring:
+    Witness the live status of your crops through three-dimensional models of your greenhouse. Sensors strategically placed in your farm provide real-time updates on temperature, humidity, soil moisture, and more. Stay connected to your farm like never before.
+    
+    2. Predictive Analysis:
+    Anticipate the future status of your farm with our predictive analysis. By analyzing current conditions and historical data, our system forecasts potential issues and suggests preemptive measures. Proactively manage your farm for optimal performance.
+    
+    3. Recommendations:
+    Receive personalized recommendations based on the real-time data and predictive analysis. Our system guides you on necessary actions to enhance the health and productivity of your crops. From adjusting environmental conditions to suggesting specific interventions, we've got you covered.</p>
+                                                                </div>
+                                                            """
+        st.markdown(html_content, unsafe_allow_html=True)
