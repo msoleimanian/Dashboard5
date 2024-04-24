db9b93d61dca68d5aafcff4b7bc13ca5419d3f16
msoleimanian <gh.solimanian@gmail.com>
2024-04-24 17:23:35 +0800

    update

--- Detection.py
+++ Detection.py
@@ -1,44 +1,41 @@
 import streamlit as st
+import streamlit as st
+from github import Github
 import os
+import boto3
 
-import streamlit.components.v1 as components
-from streamlit_option_menu import option_menu
-import pandas as pd
-import time
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
-
+access_key = 'AKIATUKO5RPGWYNFU2RN'
+Secret_access_key = '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'
 def get_download_link(file_path):
     """Generate a download link for the file."""
     return f'<a href="{file_path}" download>Click here to download the file</a>'
 
+def Detection_constructor1(file_path, bucket_name, object_key):
+    try:
+        s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=Secret_access_key)
+        s3.upload_file(file_path, bucket_name, object_key)
+        st.success("File uploaded successfully!")
+    except Exception as e:
+        st.error(f"Error uploading file: {e}")
+
+
 def Detection_constructor():
-    st.header('Weed Detection')
-    import pandas as pd
-    from io import StringIO
+    st.title("File Uploader")
 
-    uploaded_files = st.file_uploader("Choose file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        if uploaded_file is not None:
-            # Create a folder to store the uploaded data if it doesn't exist
-            if not os.path.exists("uploaded_data"):
-                os.makedirs("uploaded_data")
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        file_path = os.path.join(uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+        st.write(get_download_link(file_path), unsafe_allow_html=True)
 
-            # Save the uploaded file to the folder
-            file_path = os.path.join("uploaded_data", uploaded_file.name)
-            with open(file_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-
-            st.success("File uploaded successfully!")
-
-            # Display the path to the uploaded file
-            st.write("File path:", file_path)
-
-            # Display a download link for the uploaded file
-            st.markdown(get_download_link(file_path), unsafe_allow_html=True)
+        if st.button("Upload to AWS"):
+            Detection_constructor1(file_path, 'dtgaroimages', uploaded_file.name)
 
 
+
+    
 
 
 
